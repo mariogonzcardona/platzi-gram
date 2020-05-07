619d14f3cfbb7dc89bb8d7d345da3322d28ee506
Mario Gonzalez <33907696+mariogonzcardona@users.noreply.github.com>
2020-05-07 10:12:15 -0500

    Se agrego una aplicacion nueva posts con python manage.py startapp posts, se agregaron imagenes al archivo views.py

---
+++ posts/apps.py
@@ -0,0 +1,8 @@
+"""Posts application module."""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Posts applications settings."""
+    name = 'posts'
+    verboce_name="Posts"
