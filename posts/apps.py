d2ff0bb800f5651cc882e2cd069df70eff77cd03
Mario Gonzalez <33907696+mariogonzcardona@users.noreply.github.com>
2020-05-07 16:52:18 -0500

    Se agrego la aplicacion de users asi como profile y UserAdmin

--- posts/apps.py
+++ posts/apps.py
@@ -1,8 +1,2 @@
-"""Posts application module."""
 from django.apps import AppConfig
 
-
-class PostsConfig(AppConfig):
-    """Posts applications settings."""
-    name = 'posts'
-    verboce_name="Posts"
--- posts/views.py
+++ posts/views.py
@@ -1,39 +1,3 @@
-"""Posts views."""
-# Django
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
-
-posts=[
-    {
-        "name":"Mont Blanc",
-        "user":"Mario Gonzalez",
-        "timestamp":datetime.now().strftime("%b-%dth-%Y %H:%M:%S"),
-        "picture":'https://picsum.photos/200/200/?image=1036',
-    },
-    {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
-    },
-    {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
-    }
-]
 
 # Create your views here.
-def list_posts(request):
-    """List existing posts."""
-    content=[]
-    for post in posts:
-        content.append(
-            """
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-            """.format(**post))
-    return HttpResponse("<br>".join(content))
