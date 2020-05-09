04b9fe2fb22d864178d33eb80268fa5ebca9973c
Mario Gonzalez <33907696+mariogonzcardona@users.noreply.github.com>
2020-05-09 10:52:16 -0500

    Se agrego funcionalidad de detalle y actualizacion de posts

---
+++ posts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from posts import views
+
+urlpatterns=[
+    path(
+        route='', 
+        view=views.PostsFeedView.as_view(), 
+        name='feed'
+    ),
+    path(
+        route='posts/new/', 
+        view=views.create_post, 
+        name='create'
+        ),
+]
